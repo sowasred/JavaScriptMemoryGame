ff39d0748dda642c8fde9f2d0a55ad43a21fe62b
Ozan Muldur <36243560+sowasred@users.noreply.github.com>
2019-01-01 18:10:59 -0500

    card number is reduced

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function StartGame(option) {
   switch(option)
   {
     case "start":
-    cardValues = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    cardValues = ['A', 2, 3, 4, 5, 6]
     break
   }
 
